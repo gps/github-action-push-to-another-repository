380c3d76ab3b66117c49a8dc899fc52f8c40eab0
Kunal Gupta <kunal.g@surya-digital.com>
2023-01-18 21:04:24 +0530

    Update Actions to support SSH_DEPLOY_KEY
    - TODO:  Add steps for generating SSH_DEPLOY_KEY for target repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,15 @@ set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
 echo "[+] Action start"
-SOURCE_BEFORE_DIRECTORY="$1"
-SOURCE_DIRECTORY="$2"
-DESTINATION_GITHUB_USERNAME="$3"
-DESTINATION_REPOSITORY_NAME="$4"
-GITHUB_SERVER="$5"
-USER_EMAIL="$6"
-USER_NAME="$7"
-DESTINATION_REPOSITORY_USERNAME="$8"
-TARGET_BRANCH="$9"
+SOURCE_BEFORE_DIRECTORY="${1}"
+SOURCE_DIRECTORY="${2}"
+DESTINATION_GITHUB_USERNAME="${3}"
+DESTINATION_REPOSITORY_NAME="${4}"
+GITHUB_SERVER="${5}"
+USER_EMAIL="${6}"
+USER_NAME="${7}"
+DESTINATION_REPOSITORY_USERNAME="${8}"
+TARGET_BRANCH="${9}"
 COMMIT_MESSAGE="${10}"
 TARGET_DIRECTORY="${11}"
 GIT_TAG_NAME="${12}"
@@ -28,20 +28,51 @@ then
 	USER_NAME="$DESTINATION_GITHUB_USERNAME"
 fi
 
+# Verify that there (potentially) some access to the destination repository
+# and set up git (with GIT_CMD variable) and GIT_CMD_REPOSITORY
+if [ -n "${SSH_DEPLOY_KEY:=}" ]
+then
+	echo "[+] Using SSH_DEPLOY_KEY"
+
+	# Inspired by https://github.com/leigholiver/commit-with-deploy-key/blob/main/entrypoint.sh , thanks!
+	mkdir --parents "$HOME/.ssh"
+	DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
+	echo "${SSH_DEPLOY_KEY}" > "$DEPLOY_KEY_FILE"
+	chmod 600 "$DEPLOY_KEY_FILE"
+
+	SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+	ssh-keyscan -H "$GITHUB_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
+
+	export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+
+	GIT_CMD_REPOSITORY="git@$GITHUB_SERVER:$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+elif [ -n "${API_TOKEN_GITHUB:=}" ]
+then
+	echo "[+] Using API_TOKEN_GITHUB"
+	GIT_CMD_REPOSITORY="https://$DESTINATION_REPOSITORY_USERNAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git"
+else
+	echo "::error::API_TOKEN_GITHUB and SSH_DEPLOY_KEY are empty. Please fill one (recommended the SSH_DEPLOY_KEY)"
+	exit 1
+fi
+
 TARGET_BRANCH_EXISTS=true
 
 CLONE_DIR=$(mktemp -d)
+
+echo "[+] Git version"
+git --version
 
 echo "[+] Cloning destination git repository $DESTINATION_REPOSITORY_NAME"
 # Setup git
 git config --global user.email "$USER_EMAIL"
 git config --global user.name "$USER_NAME"
+
 { # try
-git clone --single-branch --branch "$TARGET_BRANCH" "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
+	git clone --single-branch --depth 1 --branch "$TARGET_BRANCH" "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
 } || { # on no such remote branch, pull default branch instead
-  echo "The input target branch does not already exist on the target repository. It will be created."
-git clone --single-branch "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" "$CLONE_DIR"
-  TARGET_BRANCH_EXISTS=false
+	echo "The input target branch does not already exist on the target repository. It will be created."
+	git clone --single-branch --depth 1 "$GIT_CMD_REPOSITORY" "$CLONE_DIR"
+	TARGET_BRANCH_EXISTS=false
 }
 ls -la "$CLONE_DIR"
 
@@ -51,25 +82,20 @@ TEMP_DIR=$(mktemp -d)
 # including "." and with the exception of ".git/"
 mv "$CLONE_DIR/.git" "$TEMP_DIR/.git"
 
-if [ -n "$TARGET_DIRECTORY" ]
-then
-	echo "[+] Checking if $TARGET_DIRECTORY exist in git repo $DESTINATION_REPOSITORY_NAME"
+# $TARGET_DIRECTORY is '' by default
+ABSOLUTE_TARGET_DIRECTORY="$CLONE_DIR/$TARGET_DIRECTORY/"
 
-	# Remove contents of target directory and create a new empty one
-	if [ -d "$CLONE_DIR/$TARGET_DIRECTORY/" ]
-	then
-		echo "[+] Deleting files from $TARGET_DIRECTORY in git repo $DESTINATION_REPOSITORY_NAME"
-		rm -R "$CLONE_DIR/$TARGET_DIRECTORY/"
-	fi
-	echo "[+] Creating $TARGET_DIRECTORY if doesnt already exist"
-	mkdir -p "$CLONE_DIR/$TARGET_DIRECTORY"
+echo "[+] Deleting $ABSOLUTE_TARGET_DIRECTORY"
+rm -rf "$ABSOLUTE_TARGET_DIRECTORY"
 
-	echo "[+] Listing Current Directory Location"
-	ls -al
+echo "[+] Creating (now empty) $ABSOLUTE_TARGET_DIRECTORY"
+mkdir -p "$ABSOLUTE_TARGET_DIRECTORY"
 
-	echo "[+] Listing root Location"
-	ls -al /
-fi
+echo "[+] Listing Current Directory Location"
+ls -al
+
+echo "[+] Listing root Location"
+ls -al /
 
 mv "$TEMP_DIR/.git" "$CLONE_DIR/.git"
 
@@ -120,7 +146,7 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "Pushing git commit. Create branch if none exists."
 # --set-upstream also creates the branch if it doesn't already exist in the destination repository
-git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
+git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
 
 # Createing Tag commit if Tag Name is specified
 if [ ! -z "$GIT_TAG_NAME" ]
